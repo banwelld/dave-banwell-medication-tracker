620f0c326d78f6bf34baa434113aaecb72f42ae7
Dave Banwell <dave@davebanwell.com>
2024-10-12 22:19:00 -0400

    app displays 'hello world' router working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../pages/App';
-import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(AppRoutes);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
