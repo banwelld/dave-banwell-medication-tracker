b7e8eb7c1ff1586bc4278c27857d5890f84a3e77
Dave Banwell <dave@davebanwell.com>
2024-10-12 22:53:42 -0400

    NavBar mvp

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      Hello, world!
+      <Header />
+      <Outlet />
     </div>
   );
 }
