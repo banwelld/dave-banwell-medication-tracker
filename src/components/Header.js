02dccb729dc3702b33a96c3902a5162d3219f3b6
Dave Banwell <dave@davebanwell.com>
2024-10-12 23:00:29 -0400

    renders Header mvp

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function Header() {
+    return (
+        <header>
+            <h1>My Medication Tracker</h1>
+            <NavBar />
+        </header>
+    );
+};
+
+export default Header;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Header from "./Header";
+import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Outlet />
     </div>
   );
 }
