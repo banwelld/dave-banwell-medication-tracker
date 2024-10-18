9da8d33c20b850628facf579f892993e023ecbad
Dave Banwell <dave@davebanwell.com>
2024-10-17 23:08:27 -0400

    icon added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
-import React from "react";
-import NavBar from "./NavBar";
+import React from 'react';
+import NavBar from './NavBar';
 
 function Header() {
-    return (
-        <>
-            <div className="container text-center">
-                <h1 className="mt-2">My Medication Tracker</h1>
-            </div>
-            <NavBar />
-        </>
-    );
-};
+  return (
+    <>
+      <div className='container text-center mb-4'>
+        <img
+          src='../../public/MedTrackerIcon.png'
+          alt='Medication Tracker Icon'
+        />
+        <h1 className='mt-2'>My Medication Tracker</h1>
+      </div>
+      <NavBar />
+    </>
+  );
+}
 
 export default Header;
