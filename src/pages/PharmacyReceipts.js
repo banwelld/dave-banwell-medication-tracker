e7a428379333584208ad5d631d5481d59d6efbee
Dave Banwell <dave@davebanwell.com>
2024-10-21 22:18:07 -0400

    pharmacy receipt page finalized

--- src/pages/PharmacyReceipts.js
+++ src/pages/PharmacyReceipts.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function DrugDiary(state) {
   return (
-    <div className='row m-3'>
+    <div className='row m-3 align-items-center justify-content-center'>
       <div className='col-6'>
-        <h2>How to read your pharmacy receipt</h2>
+        <h2>Reading your pharmacy receipt</h2>
         <p>
           Pharmacy receits can be confusing to read. This infographic will help
           you in gleaning the information needed to add your medications to your
