e123a501d46729a805de03152f352db0ea73e698
Dave Banwell <dave@davebanwell.com>
2024-11-13 18:17:30 -0500

    added dependency to useEffect in useDocumentTitle

--- src/hooks/useDocumentTitle.js
+++ src/hooks/useDocumentTitle.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 function useDocumentTitle(title) {
   useEffect(() => {
     document.title = `MedTracker | ${title}`;
-  }, []);
+  }, [title]);
 }
 
 export default useDocumentTitle;
